f125600a2e7ee5efd59d93d9d706a2f594b82812
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-03-14 09:19:25 +0100

    pages/views.py Added new comment

--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+# The guru is reviewing your code 
 def homepageView(request):
     return HttpResponse("Hello, World!")
